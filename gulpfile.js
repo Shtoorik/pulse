7943f80a892fc52da4384694988520d5ea767481
Shtoorik <shtoorik@mail.ru>
2022-02-15 17:35:04 +0700

    project settings done

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('styles', function () {
   return gulp.src("src/sass/**/*.+(scss|sass)")
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(rename({ suffix: '.min', prefix: '' }))
-    .pipe(autoprefixer(['last 2 versions', '> 2%'], { cascade: false }))
+    .pipe(autoprefixer())
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
